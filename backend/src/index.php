e22342867df9ef7556f169ca0eee5d1007df5a94
Morgan Lloyd <morganlloyd@Morgans-MacBook-Air.local>
2025-10-20 21:25:57 +1300

    Remove ignored files from tracking

---
+++ backend/src/index.php
@@ -0,0 +1,4 @@
+<?php
+
+
+echo '<p>hello php</p>';
---
+++ frontend/src/main.js
@@ -0,0 +1,10 @@
+
+
+const App = {
+    template: `<div>CSV Handler Frontend is running!</div>`
+};
+
+const app = document.getElementById('app');
+app.innerHTML = `
+  <h1>CSV Handler Frontend is running!</h1>
+`;
